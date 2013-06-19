ac380bd240085da2d8eeb8c6a3f265f714611418
Matt Hampel <matthew.hampel@gmail.com>
2013-06-19 16:13:47 -0400

    initial commit

---
+++ js/app/main.js
@@ -0,0 +1,61 @@
+define(['jquery', 'lodash'], function($) {
+  $(function() {
+
+    var app = {
+
+      answered: {},
+      counter: 0,
+
+      init: function() {
+        $('.in').click(app.select);
+      },
+
+      /**
+       * Select an answer
+       */
+      select: function(e) {
+        e.preventDefault();
+        var $t = $(e.target);
+        $t.closest('.question').find('.in').removeClass('selected');
+        $t.toggleClass('selected');
+
+        // Set the answer
+        app.answered[$t.attr('data-question')] = $t.text();
+
+        // If we've answered everything, submit the data.
+        if(app.ready()) {
+          app.finished();
+        }
+      },
+
+      finished: function() {
+        window.setTimeout(app.reset, 100);
+      },
+
+      /**
+       * Returns true if all three fields are complete
+       * @return {Boolean}
+       */
+      ready: function() {
+        if (app.answered.direction && app.answered.sex && app.answered.travel) {
+          return true;
+        }
+        return false;
+      },
+
+      /**
+       * Resets the fields, visually indicates we're submitting the data.
+       */
+      reset: function() {
+        console.log("Done!");
+        app.answered = {};
+        app.counter += 1;
+        $('.in').removeClass('selected');
+        $('body').addClass('done').delay(500).removeClass('done');
+      }
+
+    };
+
+    app.init();
+  });
+});
