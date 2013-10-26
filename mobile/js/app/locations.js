4d6c0bc8591ee9b56a62b54f557c8e4392ef2c87
Matt Hampel <matth@localdata.com>
2013-10-26 14:57:09 -0700

    Really fix the dates

--- mobile/js/app/locations.js
+++ mobile/js/app/locations.js
@@ -5,11 +5,11 @@ define(function (require) {
     locations: [
       {
         'name': '8 Mile Road at Dequindre',
-        'location': [-83.105361,42.418217xxx]
+        'location': [-83.083284,42.447021]
       },
       {
         'name': 'Cass at Canfield',
-        'location': [-83.064123,42.35157]
+        'location': [-83.105361,42.418217]
       },
       // {
       //   'name': 'E. Jefferson at Iroquois',
@@ -18,59 +18,55 @@ define(function (require) {
       {
         'name': 'Dequindre Cut at Atwater',
         'location': [-83.026546,42.333198]
-        ,
       },
       {
         'name': 'Gratiot at Conner Creek Greenway',
         'location': [-82.99749,42.404925]
-        ,
       },
       {
         'name': 'E. Jefferson at Van Dyke',
-        location: [-83.105206,42.417754]
+        location: [-82.994879,42.351398]
       },
       {
         'name': 'Jos Campau at Holbrook',
-        'location': [-83.105206,42.417754]
+        'location': [-83.05577,42.393697]
       },
       {
         'name': 'Livernois at Joy',
-        'location': [-83.105206,42.417754]
+        'location': [-83.138393,42.359661]
       },
       {
         'name': 'Livernois at McNichols',
-        'location': [-83.105206,42.417754]
+        'location': [-83.140714,42.417157]
       },
       {
         'name': 'Mack at Gratiot',
-        'location': [-83.105206,42.417754]
+        'location': [-83.027761,42.359035]
       },
       {
         'name': 'Michigan Ave at 14th',
-        'location': [-83.105206,42.417754]
+
+        'location': [-83.076427,42.331371]
       },
       {
         'name': 'W. Outer Drive at Grand River',
-        'location': [-83.105206,42.417754]
+        'location': [-83.232439,42.406407]
       },
       {
         'name': 'Van Dyke at Outer Drive',
-        'location': [-83.105206,42.417754]
+        'location': [-83.024499,42.441323]
       },
       {
         'name': 'W. Vernor at Springwells',
-        'location': [-83.105206,42.417754]
-        42.35157,-83.064123
-        ,
+        'location': [-83.127217, 42.311427]
       },
       {
         'name': 'Vernor at Clark',
-        'location': [-83.105206,42.35157]
-        ,-83.064123
+        'location': [-83.095814,42.320275]
       },
       {
         'name': 'Woodward north of McNichols',
-        'location': [42.418217,-83.105361]
+        'location': [-83.105206,42.417754]
       }
     ]
   };
